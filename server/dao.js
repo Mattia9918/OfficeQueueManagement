e0579b4e8a40611a4926b6690d8fa0e66b20bff5
Cihan Yurtsever <s296824@studenti.polito.it>
2022-10-18 22:41:04 +0200

    DAO Ticket get Services and Service, add Ticket, get Tickets and Ticket

--- server/dao.js
+++ server/dao.js
@@ -20,3 +20,80 @@ exports.createServiceType = (service) => {
         });
     });
 }
+
+
+// get all Services
+exports.getServices = () => {
+    return new Promise((resolve, reject) => {
+      const sql = 'SELECT * FROM SERVICE_TYPE  ORDER BY name ASC';
+      db.all(sql, [], (err, rows) => {
+        if (err)
+          reject(err);
+        else {
+          const services = rows.map(row => new SERVICE_TYPE(row.id, row.name, row.estimated_time));
+          resolve(services);
+        }
+      });
+    });
+  };
+
+
+  //get specific service type // Would be usefull(?)
+exports.getSService = (id) => {
+    return new Promise((resolve, reject) => {
+      const sql = 'SELECT * FROM SERVICE_TYPE WHERE id=?';
+      db.all(sql, [id], (err, rows) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(rows[0]);
+        }
+      });
+    });
+  };
+  
+
+/* add a new ticket */
+exports.addTicket = (ticket) => {
+    return new Promise((resolve, reject) => {
+      const sql = 'INSERT INTO TICKET( id, service_type, state, issued_at, counter) VALUES(?, ?, ?, ? ,?)'; // ID replaced as an arrangable itw // can be removed(?)
+      db.run(sql, [ticket.id, ticket.service_type, ticket.state, ticket.issued_at, ticket.counter], function (err) {
+        if (err) reject(err);
+        else resolve(this.lastID);
+      });
+    });
+  };
+
+
+
+  // get all Tickets // Would be useful(?) 
+exports.getTickets = () => {
+    return new Promise((resolve, reject) => {
+      const sql = 'SELECT * FROM TICKET ORDER BY id ASC';
+      db.all(sql, [], (err, rows) => {
+        if (err)
+          reject(err);
+        else {
+          const tickets = rows.map(row => new TICKET(row.id, row.service_type, row.state, row.issued_at, row.counter)); // ID can be removed(?)
+          resolve(tickets);
+        }
+      });
+    });
+  };
+
+
+  //get specific ticket  // Would be usefull(?) // When the ticket retrieved by customer
+exports.getSTicket = (id) => {
+    return new Promise((resolve, reject) => {
+      const sql = 'SELECT * FROM TICKET WHERE id=?';
+      db.all(sql, [id], (err, rows) => {
+        if (err) {
+  
+          reject(err);
+        } else {
+          resolve(rows[0]);
+        }
+      });
+    });
+  };
+  
