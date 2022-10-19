f5a06af33106bd6f91a7a5bc0da6028f3dbd9083
MATTIA CAMPAGNOLA s297404 <s297404@studenti.polito.it>
2022-10-19 20:55:16 +0200

    S202207OQM-30 API testing

--- server/test/prova.js
+++ server/test/prova.js
@@ -1,6 +1,5 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const { getServices } = require('../dao');
 chai.use(chaiHttp);
 chai.should();
 
@@ -23,6 +22,23 @@ describe('test service apis', () => {
         await agent.delete('/api/services/delete')
     })
 
+    const service1 = 
+    {
+        id: 1,
+        name: "service1" ,
+        estimatedTime: "30",
+    }
+
+    const service2 = 
+    {
+        id: 2,
+        name: "service2" ,
+        estimatedTime: "10",
+    }
+
+    getServices(200, service1, service2);
+
+    
 });
 
 
@@ -35,3 +51,34 @@ function postTicket(expectedHTTPStatus, serviceId) {
             });
     });
 }
+
+function getServices(expectedHTTPStatus, service1, service2) {
+    it('test get /api/service', async () => {
+        await agent.post('/api/serviceType')
+        .send(service1)
+    
+        await agent.post('/api/serviceType')
+        .send(service2)
+
+        await agent.get('/api/service')
+        .send()
+        .then(function (res) {
+            res.should.have.status(expectedHTTPStatus);
+            res.body.should.eql([
+                {
+                    "id": service1.id,
+                    "name": service1.name,
+                    "estimated_time": service1.estimatedTime
+                    
+                },
+                {
+                    "id": service2.id,
+                    "name": service2.name,
+                    "estimated_time": service2.estimatedTime
+                    
+                },
+            ]);
+        });
+    })
+}
+    
